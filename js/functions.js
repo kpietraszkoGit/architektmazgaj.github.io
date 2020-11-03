2cf5d98364721485dc16b10301f9a70da884eecf
architektmazgaj <73141073+architektmazgaj@users.noreply.github.com>
2020-11-03 02:26:29 +0100

    var and media in CSS

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+/*Functions*/
+
+$(document).ready(function() {
+var NavY = $('.navbar').offset().top;
+ 
+var stickyNav = function(){
+var ScrollY = $(window).scrollTop();
+	  
+if (ScrollY > NavY) { 
+	$('.navbar').addClass('sticky');
+} else {
+	$('.navbar').removeClass('sticky'); 
+}
+};
+ 
+stickyNav();
+ 
+$(window).scroll(function() {
+	stickyNav();
+});
+});
+
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})		
